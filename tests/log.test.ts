4309f049fb067cce8ef85c5a83717a01b236220b
Marius Szabo <szabomarius@gmail.com>
2023-10-21 14:28:26 +0300

    add some unit tests to cover scenarios

--- tests/log.test.ts
+++ tests/log.test.ts
@@ -1,24 +1,31 @@
 import { mojilogger } from "../src";
-import { DISTINCT_EMOJI_LIST } from "../src/config/config";
 
-describe('mojilogger', () => {
-  it('returns string', () => {
-    expect(mojilogger.log('log', 'Hello world')).toBeDefined();
-  });
+describe('mojilogger- withId', () => {
+    let consoleLogSpy: jest.SpyInstance;
+  
+    beforeEach(() => {
+      // Spy on console.log
+      consoleLogSpy = jest.spyOn(console, 'log').mockImplementation();
+    });
+  
+    afterEach(() => {
+      jest.resetAllMocks();
+    });
+  
+    it('logs to console', () => {
+      mojilogger.log('Hello, world!');
+      expect(consoleLogSpy).toHaveBeenCalled();
+    });
 
-  it('returns same emoji for same id', () => {
-    const id = 'bob';
-    const logs = [mojilogger.log(id, 'Hello world'), mojilogger.log(id, 'World hello')];
-    expect(logs[0]).toEqual(logs[1]);
-  });
+    it('logs to console with hardcoded emoji 💬', () => {
+        mojilogger.log('Hello, world!');
+        expect(consoleLogSpy).toHaveBeenCalledWith('💬', 'Hello, world!');
+    });
 
-  it('returns some random emoji if no more curated emojist available', () => {
-    const id = 'bob';
-    const distinctEmojisLength = DISTINCT_EMOJI_LIST.length;
-    for (let i = 0; i < distinctEmojisLength; i++) {
-      mojilogger.log(id + i, 'Hello world');
-    }
-    const newEmoji = mojilogger.log('dob', 'Hello world');
-    expect(newEmoji).toEqual(expect.stringMatching(/[\u{1F600}-\u{1F64F}]/u));
-  });
+    it('logs to console with same hardcoded emoji 💬 on multiple calls', () => {
+        mojilogger.log('Hello, world!');
+        mojilogger.log('Goodbye, planet!');
+        expect(consoleLogSpy).toHaveBeenCalledWith('💬', 'Hello, world!');
+        expect(consoleLogSpy).toHaveBeenCalledWith('💬', 'Goodbye, planet!');
+    });
 });
