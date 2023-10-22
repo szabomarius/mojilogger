00ddbf97059fe87f18c694fc86524c0f2949117f
Marius Szabo <szabomarius@gmail.com>
2023-10-22 12:48:34 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 import { NO_ID_SYMBOL } from "./config/config";
 import { isEmojiString } from "./utils/checks";
 import { getUniqueEmoji } from "./stateful/generator";
-
-type log = (id: any, message?: any, ...optionalParams: any[]) => void;
-type logOptions = {
-    id: any,
-    customEmoji?: string
-}
+import { Log, LogOptions, MojiLogger } from "./types/types";
 
 const emojiMap: Map<any, string> = new Map();
 
-function log({id , customEmoji}: logOptions, message?: any, ...optionalParams: any[]): void {
+function log({id , customEmoji}: LogOptions, message?: any, ...optionalParams: any[]): void {
     let emoji: string;
     if (customEmoji) {
         emoji = isEmojiString(customEmoji) ? customEmoji : '💬';
@@ -21,14 +16,17 @@ function log({id , customEmoji}: logOptions, message?: any, ...optionalParams: a
     console.log(emoji, message, ...optionalParams);
 }
 
-export const mojilogger = {
-    /** Binds an identifier to an emoji
-     * @param id - An identifier that gets matched with an emoji
-     * @param emoji - Optional emoji to use instead of the generated one
-     */
-    withId: (id: any, customEmoji?: string): log => log.bind(null, { id , customEmoji } ),
-    /**
-     * console logs with 💬 prepended 
-     */
-    log: log.bind(null, {id: NO_ID_SYMBOL, customEmoji: '💬'})
+export const mojilogger: MojiLogger = {
+    withId: (id: any, customEmoji?: string): Log => log.bind(null, { id , customEmoji } ),
+    log: log.bind(null, {id: NO_ID_SYMBOL, customEmoji: '💬'}),
+    mappedMojis: emojiMap,
+    setMojiList: (emojiList: string[]) => {
+        emojiMap.clear();
+        emojiList.forEach((emoji, index) => emojiMap.set(index, emoji));
+    },
+    getMojiList: () => [...emojiMap.values()],
+    resetAll: () => {
+        emojiMap.clear();
+        emojiMap.set(NO_ID_SYMBOL, '💬');
+    }
 };
